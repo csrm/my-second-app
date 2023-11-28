cbd59a9bfc6546f661deafa4bd731e75cc275efd
srchivukula <schivuku@ford.com>
2023-11-28 16:55:29 +0530

    added more comments on selector, template & styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root', //Element Selector
 //   selector: '.app-root' //Class Selector
 //   selector: '[app-root]' //Attribute Selector
-  templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',    //template from html file
+//   template: `<div class = "container"> //direct html code
+//                   <div class = "row">
+//                     <h3>I am in the App Component!!</h3>
+//                     <hr/>
+//                     <app-servers></app-servers>
+//                   </div>
+//                 </div>`,
+//   styleUrls: ['./app.component.css']  //style from a css file
   styles: [`
      h3{
      color: dodgerblue;
      }
   `]
-})
+})                                       //direct css code
 export class AppComponent {
   title = 'my-second-app';
 }
